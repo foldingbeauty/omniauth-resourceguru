17b9fff8ba8b045206f62da3c8561fab4234b4fe
Danil van Gils <daniel@steam.nl>
2013-10-17 12:07:26 +0200

    init oauth2 strategy for resource guru app

---
+++ lib/omniauth-resourceguru/version.rb
@@ -0,0 +1,5 @@
+module OmniAuth
+  module ResourceGuru
+    VERSION = "0.1.0"
+  end
+end
---
+++ lib/omniauth/strategies/resourceguru.rb
@@ -0,0 +1,23 @@
+require 'omniauth-oauth2'
+
+module OmniAuth
+  module Strategies
+    class ResourceGuru < OmniAuth::Strategies::OAuth2
+      option :name, "ResourceGuru"
+
+      option :client_options, {
+        :site => 'https://api.resourceguruapp.com/v1',
+        :authorize_url => 'https://api.resourceguruapp.com/oauth/authorize',
+        :token_url => 'https://api.resourceguruapp.com/oauth/access_token'
+      }
+
+      def accounts
+        access_token.options[:mode] = :query
+        @accounts ||= access_token.get('accounts').parsed
+      end
+
+    end
+  end
+end
+
+OmniAuth.config.add_camelization 'resourceguru', 'ResourceGuru'
